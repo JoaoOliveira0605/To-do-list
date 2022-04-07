a5f77c77fc53edcec66a6a7a454ad2790341b802
JoaoOliveira0605 <asa.noturna66@outlook.com>
2022-04-07 18:55:00 -0300

    added new forms components

--- src/pages/to-do-list/index.jsx
+++ src/pages/to-do-list/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import Form from "../../components/form";
+import TodoForm from "../../components/todoform";
+
 import { Card, Container } from "./to-do-styles";
 
 function List() {
@@ -8,7 +9,7 @@ function List() {
   return (
     <Container>
       <Card>
-        <Form></Form>
+        <TodoForm />
       </Card>
     </Container>
   );
